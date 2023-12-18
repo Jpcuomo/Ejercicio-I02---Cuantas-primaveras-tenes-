5764868566cea3e6a5c7faf5d16b00d7f6474778
Juan <jpcuomo2000@gmail.com>
2023-12-18 19:41:51 -0300

    Agregar archivos de proyecto.

---
+++ ClassLibrary1/Persona.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public class Persona
+    {
+        private string _nombre;
+        private DateTime _fechaDeNacimiento;
+        private string _dni;
+
+
+    }
+}
